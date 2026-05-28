0666796af6a7e396cc846a0146b0989af7bd0fc8
ykdman <dbsrudejr300@gmail.com>
2026-05-28 17:11:56 +0900

    Form Validation 164쪽

--- src/main/java/me/ykdman/sbb/question/QuestionController.java
+++ src/main/java/me/ykdman/sbb/question/QuestionController.java
@@ -1,8 +1,10 @@
 package me.ykdman.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,13 +30,16 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
 
     @PostMapping("/create")
-    public String questionCreate(@RequestParam(value = "subject") String subject, @RequestParam(value = "content") String content) {
-        this.questionService.create(subject, content);
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 }
